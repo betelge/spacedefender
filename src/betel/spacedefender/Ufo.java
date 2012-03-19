bbc954da67b4a4b243ce0b5c732bb4430bd1c83c
jasminko <betelge@gmail.com>
2012-03-19 12:50:00 +0000

    Added Ufo class stub.

---
+++ src/betel/spacedefender/Ufo.java
@@ -0,0 +1,14 @@
+package betel.spacedefender;
+
+import betel.alw3d.renderer.Geometry;
+import betel.alw3d.renderer.Material;
+import betel.alw3d.renderer.MovableGeometryNode;
+
+public class Ufo extends MovableGeometryNode {
+
+	public Ufo(Geometry geometry, Material material) {
+		super(geometry, material);
+		// TODO Auto-generated constructor stub
+	}
+
+}
